ef1307b46476a8874cbd68bb240c9bcbf6f0c7ba
ianduron <ianduronb@gmail.com>
2017-01-09 20:58:56 -0800

    Add files via upload

---
+++ ArithmeticPrinter.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class ArithmeticPrinter here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ArithmeticPrinter
+{
+    // instance variables - replace the example below with your own
+    public static void main(String[]args)
+    {
+        System.out.println(1+2+3+4+5);
+        System.out.println(1*2*3*4*5);
+    }
+}    
